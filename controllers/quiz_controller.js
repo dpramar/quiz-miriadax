564e50ec4a81b5467df666c61910fb924fee08a7
David Del Prado <dpramar@gmail.com>
2015-06-07 22:28:21 +0200

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -58,3 +58,28 @@ exports.create = function(req, res) {
     }
   });
 };
+
+exports.edit = function(req, res) {
+  var quiz = req.quiz;
+  console.log(quiz);
+
+  res.render('quizes/edit', {quiz: quiz, errors: []});  
+};
+
+exports.update = function(req, res) {
+  // console.log(req);
+  req.quiz.pregunta = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  req.quiz
+  .validate()
+  .then(function(err){
+    if(err) {
+      res.render('quizes/edit', {quiz: req.quiz, errors: err.errors})
+    } else {
+      req.quiz.save({fields: ['pregunta', 'respuesta']}).then(function(){
+        res.redirect('/quizes');
+      });
+    }
+  });
+};

349bc1358cf80cef6b53bb3153256a43a491ea0f
David Del Prado <dpramar@gmail.com>
2015-06-11 19:55:05 +0200

    fix

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ router.put('/quizes/:quizId(\\d+)', quizController.update);
 router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 
 router.get('/author', function(req, res, next) {
-  res.render('author', {});
+  res.render('author', {errors: []});
 });
 
 module.exports = router;
